6a9fe336b527f5357d979a541abaada783641b64
neeraj vinayakaperumal <neerajhackit@gmail.com>
2022-09-13 23:55:51 +0530

    Report and test python files pushing

--- Hello_Check.py
+++ Hello_Check.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import pytest
+
 
 dict = {"a": ["123","124"], "b" : ["456","457"]}
 df = pd.DataFrame(dict)
 print("Below is the dataframe")
 print(df)
 
+#raise Exception
---
+++ Hello_Report.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import pytest
+
+
+
+def test_01():
+    assert True
+
+
+def test_02():
+    assert True
+
+
+def test_03():
+    assert False
+
+
+def nontest_03():
+    assert False
